d6401bd78d12fd80f06d0b8a4256830f984d04a4
Arthur <s810011@gmail.com>
2013-06-16 20:13:58 +0800

    change secondBar.js to deal with tab selection.

--- _js/secondBar.js
+++ _js/secondBar.js
@@ -3,7 +3,19 @@ function processSecondBar()
 	$('#second_bar a').click(function() {
 		// hide all panels first
 		$('.panel').hide();
+		
+		// 改變被選到的tab的顏色
 		$('#second_bar a.active').removeClass('active');
 		$(this).addClass('active').blur();
+
+		// retrieve href from link (is id of panel to display)
+		var selectedPanel = $(this).attr('href');
+		// show panel
+		$(selectedPanel).show();
+
+		return false;
 	}); // end click
+
+	// 點動態牆tab, 讓一進個人首頁就看到動態牆
+	$('#second_bar a').eq(1).click();
 }
