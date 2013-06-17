9f976acf17ed5f35c9b73abd9e21d7ba1b13f935
Arthur <s810011@gmail.com>
2013-06-18 01:02:50 +0800

    在熱門群組block裡解決長title問題, 加入class="long_title"的h1

--- Home.php
+++ Home.php
@@ -60,7 +60,7 @@
                         <div class="hot-group">
 			    <a href=""> <img src="./User/Sosoman/head.jpg" width="160px" height="104px"></a>
                             <a href="">
-                            <h1>中壢高中3年13班-2002畢</h1>
+                            <div><h1 class="long_title">中壢高中3年13班-2002畢</h1></div>
 			    </a>
                             <span class="hot-group-count">47人已參加</span>
                         </div>
