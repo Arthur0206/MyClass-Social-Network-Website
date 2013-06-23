b0b176a8d11a7add0ceb4ad1bea56de6a14ebcf4
Jack Chen <s810434@gmail.com>
2013-06-24 00:22:34 +0800

    adjust like count in album panel

--- User/userMain.php
+++ User/userMain.php
@@ -23,37 +23,37 @@
         <script>
             $(document).ready(function() {
                 // Process Menu Bar
-		processMenuBar();
-
-		// Process User Background Area
-		preocessUserBackground();
-
-		// Process Second Bar
-		processSecondBar();
-
-		// Process Setting Panel
-		processSettingPanel();
-
-		// Process About Panel
-		processAboutPanel();
-
-		// Process Wall Panel
-		processWallPanel();
-
-		// Process Album Panel
-		processAlbumPanel();
-
-		// Process Fancybox
-		$('#gallery a').fancybox({
-			overlayColor: '#666',
-			overlayOpacity: .05,
-			transitionIn: 'elastic',
-			transitionOut: 'elastic',
-			//easingIn: 'easeInSine', 省去不必須的下載(easing.js)
-			//easingOut: 'easeOutSine',
-			titlePosition: 'inside' ,		
-			cyclic: true
-		});
+				processMenuBar();
+        		
+				// Process User Background Area
+				preocessUserBackground();
+        		
+				// Process Second Bar
+				processSecondBar();
+        		
+				// Process Setting Panel
+				processSettingPanel();
+        		
+				// Process About Panel
+				processAboutPanel();
+        		
+				// Process Wall Panel
+				processWallPanel();
+        		
+				// Process Album Panel
+				processAlbumPanel();
+        		
+				// Process Fancybox
+				$('#gallery a').fancybox({
+					overlayColor: '#666',
+					overlayOpacity: .05,
+					transitionIn: 'elastic',
+					transitionOut: 'elastic',
+					//easingIn: 'easeInSine', 省去不必須的下載(easing.js)
+					//easingOut: 'easeOutSine',
+					titlePosition: 'inside' ,		
+					cyclic: true
+				});
             }); // ready
         </script>
     </head>
