eda0acd2e9c804083cac8b0963bed3262cd0273c
Jack Chen <s810434@gmail.com>
2013-06-23 21:39:01 +0800

    Sosoman 相簿初步

--- User/albumPanel.php
+++ User/albumPanel.php
@@ -4,33 +4,44 @@
 		<span id="add_new_moment_button" class="add_search_button unselectable">＋新增相簿</span>
 	</div>
 	<div class="albums_continer">
-		<a class="album-image-link" href="sosoman's album #1">
-			<div class="album">
+		<div class="album">
+			<a class="album-image-link" href="sosoman's album #1">
 				<img class="album-image" alt="" src="Sosoman/Album/1/oreo.jpg">
 				<div class="album-headline-wrapper"> 
 					<h1 class="album-headline">我的餅乾</h1>
 				</div>
 				<div class="album-info-wrapper"> 
 					<ul class="album-info">
-						<li class="album-create-time">2010年5月3日</li>
-						<li class="facebook-like">
-							<span class="like-icon"></span>
+						<li class="create-time">2013年5月3日</li>
+						<li class=like-count>
+							64
 						</li>
-						<li class="myclass-like">
-							<img src="../Pics/heart.png">
-						</li>
-						<li class="comment">
-							<imp src="">
+						<li class="like">
+							<img class="like-icon" alt="like" src="../Pics/icon_like.png">
 						</li>
 					</ul>
 				</div>
-			</div>
-		</a>
+			</a>
+		</div>
 	</div>
 	<div class="albums_continer">
 		<div class="album">
 			<a class="album-image-link" href="sosoman's album #2">
 				<img class="album-image" alt="" src="Sosoman/Album/2/player.jpg">
+				<div class="album-headline-wrapper"> 
+					<h1 class="album-headline">雜七雜八的東西</h1>
+				</div>
+				<div class="album-info-wrapper"> 
+					<ul class="album-info">
+						<li class="create-time">2013年1月17日</li>
+						<li class=like-count>
+							35
+						</li>
+						<li class="like">
+							<img class="like-icon" alt="like" src="../Pics/icon_like.png">
+						</li>
+					</ul>
+				</div>
 			</a>
 		</div>
 	</div>
@@ -38,6 +49,20 @@
 		<div class="album">
 			<a class="album-image-link" href="sosoman's album #3">
 				<img class="album-image" alt="" src="Sosoman/Album/3/geek.jpg">
+				<div class="album-headline-wrapper"> 
+					<h1 class="album-headline">Geeks</h1>
+				</div>
+				<div class="album-info-wrapper"> 
+					<ul class="album-info">
+						<li class="create-time">2012年9月27日</li>
+						<li class=like-count>
+							107
+						</li>
+						<li class="like">
+							<img class="like-icon" alt="like" src="../Pics/icon_like.png">
+						</li>
+					</ul>
+				</div>
 			</a>
 		</div>
 	</div>
@@ -45,6 +70,20 @@
 		<div class="album">
 			<a class="album-image-link" href="sosoman's album #4">
 				<img class="album-image" alt="" src="Sosoman/Album/4/telly-ios.jpg">
+				<div class="album-headline-wrapper"> 
+					<h1 class="album-headline">手機拍的照片</h1>
+				</div>
+				<div class="album-info-wrapper"> 
+					<ul class="album-info">
+						<li class="create-time">2012年8月8日</li>
+						<li class=like-count>
+							88
+						</li>
+						<li class="like">
+							<img class="like-icon" alt="like" src="../Pics/icon_like.png">
+						</li>
+					</ul>
+				</div>
 			</a>
 		</div>
 	</div>
@@ -52,6 +91,20 @@
 		<div class="album">
 			<a class="album-image-link" href="sosoman's album #5">
 				<img class="album-image" alt="" src="Sosoman/Album/5/book.jpg">
+				<div class="album-headline-wrapper"> 
+					<h1 class="album-headline">我的作品</h1>
+				</div>
+				<div class="album-info-wrapper"> 
+					<ul class="album-info">
+						<li class="create-time">2012年2月6日</li>
+						<li class=like-count>
+							26
+						</li>
+						<li class="like">
+							<img class="like-icon" alt="like" src="../Pics/icon_like.png">
+						</li>
+					</ul>
+				</div>
 			</a>
 		</div>
 	</div>
@@ -59,6 +112,20 @@
 		<div class="album">
 			<a class="album-image-link" href="sosoman's album #6">
 				<img class="album-image" alt="" src="Sosoman/Album/Moment/7_b.jpg">
+				<div class="album-headline-wrapper"> 
+					<h1 class="album-headline">Moment</h1>
+				</div>
+				<div class="album-info-wrapper"> 
+					<ul class="album-info">
+						<li class="create-time">2010年4月21日</li>
+						<li class=like-count>
+							421
+						</li>
+						<li class="like">
+							<img class="like-icon" alt="like" src="../Pics/icon_like.png">
+						</li>
+					</ul>
+				</div>
 			</a>
 		</div>
 	</div>
