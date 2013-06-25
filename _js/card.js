8b875c6eff80f8fbc25e3d5352dcecd482504437
Jack Chen <s810434@gmail.com>
2013-06-25 17:27:56 +0800

    1. add icons 2. change panel css

--- _js/card.js
+++ _js/card.js
@@ -1,10 +1,10 @@
 function processCard() {
 	$('.card').hover(
 		function() {
-			$(this).css({'background-color': '#F4F4F4', 'border-color': '#C1C1C1'}).find('.album-image').css('border-color', '#C1C1C1');
+			$(this).css({'background-color': '#F4F4F4', 'border-color': '#C1C1C1'}).find('.card-image').css('border-color', '#C1C1C1');
 		},
 		function() {
-			$(this).css({'background-color': '#FFFFFF', 'border-color': '#E1E1E1'}).find('.album-image').css('border-color', '#E1E1E1');
+			$(this).css({'background-color': '#FFFFFF', 'border-color': '#E1E1E1'}).find('.card-image').css('border-color', '#E1E1E1');
 		}
 	); //end hover
 
