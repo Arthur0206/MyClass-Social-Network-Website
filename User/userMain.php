40a0fd71fab500a237d0696abebd936ca480c7f1
Jack Chen <s810434@gmail.com>
2013-07-19 13:14:19 +0800

    trivial

--- User/userMain.php
+++ User/userMain.php
@@ -81,95 +81,92 @@ if (!isset($_SESSION)) {
     </head>
     <body>
 	<div style="margin:auto;box-shadow: 0px 0px 2px 1px #D8D5D0;background:url('../Pics/background.png')">
-	<?php 
-	/* include top menu bar */ 
-	require_once('../menubar.php'); 
-	?>
-    <div id="user_background" class="boy_user_background">
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/tall_pic.jpg" width="180px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/wide_pic_4.jpg" width="280px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/girl3.jpg" width="250px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/wide_pic.jpg" width="290px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/wide_pic_2.jpg" width="280px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/nature.jpg" width="250px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/wide_pic_3.jpg" width="290px" height="200px">
-            </a>
-        </div>
-        <div class="user_back_img_block">
-            <a href="">
-                <img src="../Pics/girl4.jpg" width="180px" height="200px">
-            </a>
-        </div>
-		<div id="user_profile_block">
-            <a href="">
-		        <img class="user_profile_img" src="../Pics/girl5.jpg">
-            </a>
-    		<div class="user_profile_text">
-                <div>
-                    Puff001
-                </div>
-                <div>
-                    郭雪芙
-                </div>
+	    <?php 
+	    /* include top menu bar */ 
+	    require_once('../menubar.php'); 
+	    ?>
+        <div id="user_background" class="boy_user_background">
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/tall_pic.jpg" width="180px" height="200px">
+                </a>
             </div>
-		</div>
-
-		<!--<span id="hide_background_button" style="position:absolute;left:46%;bottom:1px;color:#333333;background-color:#edebe2;padding:5px 10px;border-radius:10px 10px 0 0;box-shadow:0px 0px 2px 1px #acacac;cursor:pointer">最小化背景圖片</span>-->
-	</div>
-	<div id="second_bar">
-		<ul>
-			<li><a href="#about_panel"><img src="../Pics/Paper.png" width=30px>個人資料</a></li>
-			<li><a href="#wall_panel"><img src="../Pics/profile.png" width=30px>動態牆</a></li>
-			<li><a href="#album_panel"><img src="../Pics/ticket-icon.png" width=30px>相簿</a></li>
-			<li><a href="#friend_panel"><img src="../Pics/notes-edit.png" width=30px>朋友</a></li>
-			<li><a href="#group_panel"><img src="../Pics/pen-2.png" width=30px>群組</a></li>
-			<li><a href="#event_panel"><img src="../Pics/calendar-icon.png" width=30px>活動</a></li>
-			<li><a href="#article_panel"><img src="../Pics/checklist-2.png" width=30px>文章</a></li>
-			<li><a href="#board_panel"><img src="../Pics/list-plus.png" width=30px>討論版</a></li>
-            <li><a href="#board_panel"><img src="../Pics/setting.png" width=30px>設定</a></li>
-		</ul>
-	</div>
-    <div id="panel_continer">
-	<?php 
-	    require_once('aboutPanel.php'); 
-	    require_once('wallPanel.php'); 
-	    require_once('albumPanel.php'); 
-	    require_once('friendPanel.php'); 
-	    require_once('groupPanel.php'); 
-	    require_once('eventPanel.php'); 
-	    require_once('articlePanel.php'); 
-	    require_once('boardPanel.php'); 
-	?> 
-<<<<<<< HEAD
-	</div>
-=======
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/wide_pic_4.jpg" width="280px" height="200px">
+                </a>
+            </div>
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/girl3.jpg" width="250px" height="200px">
+                </a>
+            </div>
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/wide_pic.jpg" width="290px" height="200px">
+                </a>
+            </div>
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/wide_pic_2.jpg" width="280px" height="200px">
+                </a>
+            </div>
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/nature.jpg" width="250px" height="200px">
+                </a>
+            </div>
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/wide_pic_3.jpg" width="290px" height="200px">
+                </a>
+            </div>
+            <div class="user_back_img_block">
+                <a href="">
+                    <img src="../Pics/girl4.jpg" width="180px" height="200px">
+                </a>
+            </div>
+	    	<div id="user_profile_block">
+                <a href="">
+	    	        <img class="user_profile_img" src="../Pics/girl5.jpg">
+                </a>
+        		<div class="user_profile_text">
+                    <div>
+                        Puff001
+                    </div>
+                    <div>
+                        郭雪芙
+                    </div>
+                </div>
+	    	</div>
+        
+	    	<!--<span id="hide_background_button" style="position:absolute;left:46%;bottom:1px;color:#333333;background-color:#edebe2;padding:5px 10px;border-radius:10px 10px 0 0;box-shadow:0px 0px 2px 1px #acacac;cursor:pointer">最小化背景圖片</span>-->
+	    </div>
+	    <div id="second_bar">
+	    	<ul>
+	    		<li><a href="#about_panel"><img src="../Pics/Paper.png" width=30px>個人資料</a></li>
+	    		<li><a href="#wall_panel"><img src="../Pics/profile.png" width=30px>動態牆</a></li>
+	    		<li><a href="#album_panel"><img src="../Pics/ticket-icon.png" width=30px>相簿</a></li>
+	    		<li><a href="#friend_panel"><img src="../Pics/notes-edit.png" width=30px>朋友</a></li>
+	    		<li><a href="#group_panel"><img src="../Pics/pen-2.png" width=30px>群組</a></li>
+	    		<li><a href="#event_panel"><img src="../Pics/calendar-icon.png" width=30px>活動</a></li>
+	    		<li><a href="#article_panel"><img src="../Pics/checklist-2.png" width=30px>文章</a></li>
+	    		<li><a href="#board_panel"><img src="../Pics/list-plus.png" width=30px>討論版</a></li>
+                <li><a href="#board_panel"><img src="../Pics/setting.png" width=30px>設定</a></li>
+	    	</ul>
+	    </div>
+        <div id="panel_continer">
+	    <?php 
+	        require_once('aboutPanel.php'); 
+	        require_once('wallPanel.php'); 
+	        require_once('albumPanel.php'); 
+	        require_once('friendPanel.php'); 
+	        require_once('groupPanel.php'); 
+	        require_once('eventPanel.php'); 
+	        require_once('articlePanel.php'); 
+	        require_once('boardPanel.php'); 
+	    ?> 
+	    </div>
     </div>
->>>>>>> 7287cc996d7389b7b55d589470ccdce04d63bd0d
     </body>
 </html>
